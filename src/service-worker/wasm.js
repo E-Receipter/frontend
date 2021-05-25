a3ac98611479d8cbc5b478e904bf9cfa16f8877e
dravog7 <dravog77@gmail.com>
2021-05-25 18:00:43 +0530

    fixed WASM

--- src/service-worker/wasm.js
+++ src/service-worker/wasm.js
@@ -4,16 +4,12 @@ let module = null;
 
 export function importWASM(importer){
     importer(WASM_MODULE_JS);
-    console.log(self.Module);
     importer(JABCODEREADER);
-    loadWASM();
 }
 
 async function loadWASM(){
     const req = await fetch(WASM_MODULE);
-    console.log(req);
     const buffer = await req.arrayBuffer();
-    console.log(buffer.byteLength);
     module = await self.Module(
         {
             wasmBinary: buffer,
@@ -22,5 +18,7 @@ async function loadWASM(){
 }
 
 export async function handleScan({url, request, event, params}){
+    if(!module)
+        await loadWASM();
     return new Response(module._test(10,20).toString());
 }
